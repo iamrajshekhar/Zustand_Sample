e6b1542ad6e4b62e81255430e739e69100838c0f
iamrajshekhar <rajshekhar.yadav@flyhomes.com>
2023-02-05 12:23:28 +0530

    Setting Up

--- App.js
+++ App.js
@@ -17,10 +17,7 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
+
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
